37ccdbd8cf10495615e0d758f10a8cb672280463
sumanverma8107 <sumanverma8107@gmail.com>
2026-02-07 11:50:57 +0530

    Initial commit

---
+++ src/components/CreatePost.jsx
@@ -0,0 +1,125 @@
+import { useContext, useRef } from "react";
+import { PostList } from "../store/post-list-store";
+import { useNavigate } from "react-router-dom";
+
+const CreatePost = () => {
+  const { addPost } = useContext(PostList);
+  const navigate = useNavigate();
+
+  const userIdElement = useRef();
+  const postTitleElement = useRef();
+  const postBodyElement = useRef();
+  const reactionsElement = useRef();
+  const tagsElement = useRef();
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    const userId = userIdElement.current.value;
+    const postTitle = postTitleElement.current.value;
+    const postBody = postBodyElement.current.value;
+    const reactions = Number(reactionsElement.current.value);
+    const tags = tagsElement.current.value.split(" ");
+
+    userIdElement.current.value = "";
+    postTitleElement.current.value = "";
+    postBodyElement.current.value = "";
+    reactionsElement.current.value = "";
+    tagsElement.current.value = "";
+
+    fetch("https://dummyjson.com/posts/add", {
+      method: "POST",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify({
+        id: Date.now(),
+        title: postTitle,
+        body: postBody,
+        reactions: {
+          likes: reactions,
+        },
+        userId: userId,
+        tags: tags,
+      }),
+    })
+      .then((res) => res.json())
+      .then((post) => {
+        addPost(post);
+        navigate("/");
+      });
+  };
+
+  return (
+    <form className="create-post" onSubmit={handleSubmit}>
+      <div className="mb-3">
+        <label htmlFor="userId" className="form-label">
+          User Id
+        </label>
+        <input
+          type="text"
+          className="form-control"
+          ref={userIdElement}
+          id="userId"
+          placeholder="Enter your user id"
+        />
+      </div>
+
+      <div className="mb-3">
+        <label htmlFor="title" className="form-label">
+          Post Title
+        </label>
+        <input
+          type="text"
+          className="form-control"
+          ref={postTitleElement}
+          id="title"
+          placeholder="How are you Feeling today..."
+        />
+      </div>
+
+      <div className="mb-3">
+        <label htmlFor="body" className="form-label">
+          Post Content
+        </label>
+        <textarea
+          type="text"
+          className="form-control"
+          rows={4}
+          ref={postBodyElement}
+          id="body"
+          placeholder="Tell us more about it"
+        />
+      </div>
+
+      <div className="mb-3">
+        <label htmlFor="reactions" className="form-label">
+          Number of Reactions
+        </label>
+        <input
+          type="text"
+          className="form-control"
+          ref={reactionsElement}
+          id="reactions"
+          placeholder="How many people reacted to this post"
+        />
+      </div>
+
+      <div className="mb-3">
+        <label htmlFor="tags" className="form-label">
+          Enter your hashtags here
+        </label>
+        <input
+          type="text"
+          className="form-control"
+          ref={tagsElement}
+          id="tags"
+          placeholder="Please enter tags using space"
+        />
+      </div>
+
+      <button type="submit" className="btn btn-primary">
+        Post
+      </button>
+    </form>
+  );
+};
+
+export default CreatePost;
---
+++ src/components/WelcomeMessage.jsx
@@ -0,0 +1,9 @@
+const WelcomeMessage = () => {
+  return (
+    <center className="welcome-message">
+      <h1>There are no Post</h1>
+    </center>
+  );
+};
+
+export default WelcomeMessage;
